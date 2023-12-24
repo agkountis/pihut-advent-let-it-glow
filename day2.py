9e38cac54792b4d2c7eb6068f52fbf8e4ea614dc
Angelos Gkountis <angelos.gkountis@gmail.com>
2023-12-24 21:08:03 +0000

    Now using a ranged for loop to blink the LED.

--- day2.py
+++ day2.py
@@ -5,10 +5,13 @@ import time
 # Pin setup
 red = Pin(14, Pin.OUT) # Set GPIO as an output
 
-while True: # Loop forever
+for i in range(10): # Loop 10 times
     red.value(1) # LED on
     time.sleep(0.5) # Wait half a second.
     
     red.value(0) # LED off
     time.sleep(0.5)	# Wait half a second.
+    
+    print("Flash!")
 
+print("Program finished")
